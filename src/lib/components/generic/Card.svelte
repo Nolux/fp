f8f56b3ff1fb49dc9931e98f524f8ee080b3819a
Atle Selbek <atle.selbek@gmail.com>
2025-09-24 23:45:11 +0200

    feat: add chores localization and home page layout
    - Add "chores" entry to English localization file for improved internationalization support.
    - Create a new home page layout with a grid of cards for displaying content.

---
+++ src/lib/components/generic/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let {
+    children,
+    cols,
+    rows,
+    color,
+    className,
+  }: {
+    children: any;
+    cols?: string;
+    rows?: string;
+    className?: string;
+    color?: string;
+  } = $props();
+</script>
+
+<div
+  class={`border-3 border-${color} shadow-xl shadow-${color || "slate-500"} rounded-xl p-4 ${cols ? `col-span-${cols}` : ""} ${rows ? `row-span-${rows}` : ""} ${className ? className : ""}`}
+>
+  {@render children?.()}
+</div>
---
+++ src/routes/(protected)/home/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Card from "$lib/components/generic/Card.svelte";
+</script>
+
+<div class="grid grid-cols-5 grid-rows-3 h-full gap-8 p-10">
+  <Card cols="1" rows="full" color="primary">12341123</Card>
+  <Card cols="3" rows="full" color="secondary">12341123</Card>
+  <Card cols="1" rows="full" color="warning">12341123</Card>
+</div>
